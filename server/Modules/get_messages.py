536d492250f6f3935f4287417a5bd7f6330deae3
Andrew White <dwhite03@byui.edu>
2025-10-25 12:29:40 -0600

    Update get_messages.py

--- server/Modules/get_messages.py
+++ server/Modules/get_messages.py
@@ -2,7 +2,18 @@ import os
 import json
 
 def get_messages(user_uuid:str, conversation_uuid:str):
-    convo_path = os.path.join("users", user_uuid, "conversations", conversation_uuid, "coversation.json")
-
-    with open(convo_path, "r") as f:
-        return json.load(f)["messages"]
+    users = os.listdir("server/users/")
+    if user_uuid not in users:
+        return None
+    else:
+        conversations = os.listdir(f"server/users/{user_uuid}/conversations/")
+        if conversation_uuid not in conversations:
+            return None
+        else:
+            os.listdir(f"server/users/{user_uuid}/conversations/{conversation_uuid}/")
+            if "conversation.json" not in os.listdir(f"server/users/{user_uuid}/conversations/{conversation_uuid}/"):
+                return None
+            else:
+                with open(os.path.join("server", "users", user_uuid, "conversations", conversation_uuid, "conversation.json"), "r", encoding="utf-8") as f:
+                    data = json.load(f)
+                    return data.get("messages", [])
